2bbdc579cfcb7c92904094e980ec4a4dcad0ad72
Ashutosh Singh <ash9926157103@gmail.com>
2023-08-11 00:06:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -39,20 +39,20 @@ function App() {
 
   return (
     <>
-      {/* <Router> */}
-      <Navbar title="TextUtils" mode={mode} toogleMode={darkModeBtn} />
-      <Alert alert={alert} />
-      <div className='container my-3'>
-        {/* <Routes> */}
-        {/* <Route exact path="/about" element={ */}
-        {/* <About /> */}
-        {/* } /> */}
-        {/* <Route exact path="/" element={ */}
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-        {/* }/> */}
-        {/* </Routes> */}
-      </div>
-      {/* </Router> */}
+      <Router>
+        {/* <Navbar title="TextUtils" mode={mode} toogleMode={darkModeBtn} /> */}
+        <Alert alert={alert} />
+        <div className='container my-3'>
+          <Routes>
+            <Route exact path="/about" element={
+              <About />
+            } />
+            <Route exact path="/" element={
+              <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+            } />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
